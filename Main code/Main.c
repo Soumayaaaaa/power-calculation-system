1eedf2bb1c783eb2c60d36a83b2d75a28df4f70f
Soumaya <souumaayaaa@gmail.com>
2026-05-20 03:09:10 +0100

    made sleep work on linux

--- Main code/Main.c
+++ Main code/Main.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#ifdef _WIN32
 #include <windows.h>
+#else
+#include <unistd.h>
+#endif
 #include "Vampire.h"
 
 void wait_ms(int ms) {
+#ifdef _WIN32
     Sleep(ms);
+#else
+    usleep(ms * 1000);
+#endif
 }
 
 void loading() {
